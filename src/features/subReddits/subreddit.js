f95a8f549817aa220bd5520ae17feecf4eec60f9
Alice Tonkopiy <adtonkopiy@gmail.com>
2022-10-24 16:58:17 +0200

    Add default Subreddit pic if it fails to load

--- src/features/subReddits/subreddit.js
+++ src/features/subReddits/subreddit.js
@@ -8,16 +8,22 @@ import { Postslist } from "../posts/postsList";
 */
 
 export const Subreddit = ({ subreddit, children }) => {
-  const handleClick = () => {};
+
+    const thumbnail = subreddit.data.thumbnail;
+    
+    const handleClick = () => {};
 
   return (
     <div>
       <button style={{ height: "80px", width: "200px" }} onClick={handleClick}>
         <img
-          src={subreddit.data.thumbnail}
-          onError={() => console.log(this)}
+          src={thumbnail}
           alt="thumbnail"
           style={{ height: "50px", width: "50px", borderRadius: "50px" }}
+          onError={event => {
+            event.target.src = "https://external-preview.redd.it/iDdntscPf-nfWKqzHRGFmhVxZm4hZgaKe5oyFws-yzA.png?width=640&crop=smart&auto=webp&s=bfd318557bf2a5b3602367c9c4d9cd84d917ccd5"
+            event.onerror = null
+          }}
         />
         {subreddit.data.subreddit}
       </button>
